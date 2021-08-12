9de64c278ee8c6a3e3181f486ce7dded673e8036
Bryan <bryan.zamora@goitsa.me>
2021-08-12 12:29:51 -0500

    Se agrega select de lenguaje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule     
+    ReactiveFormsModule ,
+    MatSelectModule,
+    BrowserAnimationsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ export class LoginComponent implements OnInit {
 
   usuario:String="";
   contrasena:String="";
+  selected:String='es';
+  langArray:any;
 
   constructor() { 
     
@@ -51,6 +53,58 @@ export class LoginComponent implements OnInit {
             $("#contrasena").css("border-bottom","1px solid blue");
         }
     });
+
+    //test for getting url value from attr
+// var img1 = $('.test').attr("data-thumbnail");
+// console.log(img1);
+
+//test for iterating over child elements
+$('.vodiapicker option').each((a:any) =>{
+  var img = $(a).attr("data-thumbnail");
+  var text = a.innerText;
+  var value = $(a).val();
+  var item = '<li><img src="'+ img +'" alt="" value="'+value+'"/><span>'+ text +'</span></li>';
+  this.langArray.push(item);
+})
+
+$('#a').html(this.langArray);
+
+//Set the button value to the first el of the array
+$('.btn-select').html(this.langArray[0]);
+$('.btn-select').attr('value', 'en');
+
+//change button stuff on click
+$('#a li').click(function(resp:any){
+   var img = $(resp).find('img').attr("src");
+   var value = $(resp).find('img').attr('value');
+   var text = resp.innerText;
+   var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
+  $('.btn-select').html(item);
+  $('.btn-select').attr('value', value);
+  $(".b").toggle();
+  //console.log(value);
+});
+
+$(".btn-select").click(function(){
+        $(".b").toggle();
+    });
+
+//check local storage for the lang
+var sessionLang = localStorage.getItem('lang');
+if (sessionLang){
+  //find an item with value of sessionLang
+  var langIndex = this.langArray.indexOf(sessionLang);
+  $('.btn-select').html(this.langArray[langIndex]);
+  $('.btn-select').attr('value', sessionLang);
+} else {
+   var langIndex = this.langArray.indexOf('ch');
+  console.log(langIndex);
+  $('.btn-select').html(this.langArray[langIndex]);
+  //$('.btn-select').attr('value', 'en');
+}
+
+
+
   }
 
 }
