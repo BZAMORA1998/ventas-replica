f2d0b2bfd2f837f5bc15ef67e07b6e74af6e01a5
Bryan <bryan.zamora@goitsa.me>
2021-08-12 16:14:27 -0500

    Se valida campos vacios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,8 +9,8 @@ declare let $: any;
 })
 export class LoginComponent implements OnInit {
 
-  usuario:String="";
-  contrasena:String="";
+  usuario:any=null;
+  contrasena:any=null;
   langArray:any;
 
   public activeLang = 'es';
@@ -62,58 +62,16 @@ export class LoginComponent implements OnInit {
             $("#contrasena").css("border-bottom","1px solid blue");
         }
     });
-
-    //test for getting url value from attr
-// var img1 = $('.test').attr("data-thumbnail");
-// console.log(img1);
-
-//test for iterating over child elements
-$('.vodiapicker option').each((a:any) =>{
-  var img = $(a).attr("data-thumbnail");
-  var text = a.innerText;
-  var value = $(a).val();
-  var item = '<li><img src="'+ img +'" alt="" value="'+value+'"/><span>'+ text +'</span></li>';
-  this.langArray.push(item);
-})
-
-$('#a').html(this.langArray);
-
-//Set the button value to the first el of the array
-$('.btn-select').html(this.langArray[0]);
-$('.btn-select').attr('value', 'en');
-
-//change button stuff on click
-$('#a li').click(function(resp:any){
-   var img = $(resp).find('img').attr("src");
-   var value = $(resp).find('img').attr('value');
-   var text = resp.innerText;
-   var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
-  $('.btn-select').html(item);
-  $('.btn-select').attr('value', value);
-  $(".b").toggle();
-  //console.log(value);
-});
-
-$(".btn-select").click(function(){
-        $(".b").toggle();
-    });
-
-//check local storage for the lang
-var sessionLang = localStorage.getItem('lang');
-if (sessionLang){
-  //find an item with value of sessionLang
-  var langIndex = this.langArray.indexOf(sessionLang);
-  $('.btn-select').html(this.langArray[langIndex]);
-  $('.btn-select').attr('value', sessionLang);
-} else {
-   var langIndex = this.langArray.indexOf('ch');
-  console.log(langIndex);
-  $('.btn-select').html(this.langArray[langIndex]);
-  //$('.btn-select').attr('value', 'en');
-}
-
-
-
   }
 
+  validaCamposVacios(){
+
+    if(this.usuario!="" && this.contrasena!="" && this.usuario!=null && this.contrasena!=null){
+      console.log(true);
+      $("#btn-ok").prop('disabled',false);
+    }else{
+      console.log(false);
+      $("#btn-ok").prop('disabled',true);
+    }
+  }
 }
