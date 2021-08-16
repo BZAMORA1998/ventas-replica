159d96b9f274b4d5102137c52de878043276d5bf
Bryan <bryan.zamora@goitsa.me>
2021-08-15 20:20:13 -0500

    Se agrega componente de modulos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,25 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { SeguridadComponent } from './seguridad/seguridad.component';
 import { ModulosComponent } from './modulos/modulos.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 
 @NgModule({
   declarations: [SeguridadComponent, ModulosComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [ HttpClient ]
+      }
+    })  
   ]
 })
 export class HomeModule { }
--- src/app/home/modulos/modulos.component.ts
+++ src/app/home/modulos/modulos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-modulos',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModulosComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _translate: TranslateService,) { }
 
   ngOnInit(): void {
   }
+
+  modulos: any[] = [
+    {
+      "name": "SEGURIDAD",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "PRODUCTOS",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "COMPRAS",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "VENTAS",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "MOVIMIENTO DE CAJAS",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "DEVOLUCIONES",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "KARDEX",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    },
+    {
+      "name": "REPORTES",
+      "registro":"11 REGISTRADA",
+      "img":"fa fa-unlock fa-3x"
+    }
+  ];
 
 }
