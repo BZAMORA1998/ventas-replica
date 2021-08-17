14b8b0fdae586d95bee812884ced45545e8719e5
Bryan <bryan.zamora@goitsa.me>
2021-08-17 18:03:55 -0500

    Se ajusta componente text, los focus no salian correctamente

--- src/app/util/text/text.component.ts
+++ src/app/util/text/text.component.ts
@@ -11,25 +11,29 @@ export class TextComponent implements OnInit {
   constructor() { }
   @Input() label: string;
   @Input() icon: string;
-  text:string="";
-  @Output() texto=new EventEmitter<string>();
+  @Input() id: number=0;
+  @Input() textInt:string="";
+  @Output() textOut=new EventEmitter<string>();
   
   ngOnInit(): void {
-    $("#textoI").focus(function() {
-      $("#textoF").css("margin-top",-20);
-      $("#textoI").css("border-bottom","1px solid blue");
-      $("#textoF").css("font-size","12px");
-    });
-     $("#textoI").blur(() => {
-        if(this.text==""){
-            $("#textoF").css("margin-top",0);
-            $("#textoI").css("border-bottom","1px solid red");
-            $("#textoF").css("font-size","13px");
-        }
-    });
+
   }
 
   setText() {
-    this.texto.emit(this.text);
+    this.textOut.emit(this.textInt);
+  }
+  onFocusEvent(){
+    console.log("Entro");
+      $(`#textoF-${this.id}`).css("margin-top",-20);
+      $(`#textoI-${this.id}`).css("border-bottom","1px solid blue");
+      $(`#textoF-${this.id}`).css("font-size","12px");
+  }
+  onFocusOutEvent(){
+    console.log("salio");
+    if(this.textInt==""){
+      $(`#textoF-${this.id}`).css("margin-top",0);
+      $(`#textoI-${this.id}`).css("border-bottom","1px solid red");
+      $(`#textoF-${this.id}`).css("font-size","13px");
+    }
   }
 }
