cd5a3f4ecc3deda8d2092c8a5d0755fc3444570e
Bryan <bryan.zamora@goitsa.me>
2021-08-19 17:38:48 -0500

    Se agrega descripcion a las apis que se van a consultar

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -7,43 +7,82 @@ import { ApiService } from './api.service';
   providedIn: 'root'
 })
 export class UsuarioService {
+
+  constructor(private apiService: ApiService,private http: HttpClient){}
+
+
+  /**
+   * @description Consulta los usuarios de manera general
+   * 
+   * @author Bryan Zamora
+   * @param page 
+   * @param perPage 
+   * @param valor 
+   * @param estado 
+   * @returns 
+   */
+  getConsultaUsuario(page,perPage,valor,estado):Observable<any>{
+    return this.apiService.ApiCallSpring("GET",`/usuarios?page=${page}&perPage=${perPage}&cedulaCodigoUsuario=${valor}&estado=${estado}`,null,null);
+  }
   
+   /**
+   * @description Elimina el usuario
+   * 
+   * @author Bryan Zamora
+   * @param secuenciaUsuario 
+   * @returns 
+   */
   eliminarUsuario(secuenciaUsuario: any) {
     return this.apiService.ApiCallSpring("DELETE",`/usuarios/${secuenciaUsuario}/eliminarUsuario`,null,null);
   }
-  putActivarOInactivarUsuario(secuenciaUsuario: any) {
-    return this.apiService.ApiCallSpring("PUT",`/usuarios/activarOInactivarUsuario/${secuenciaUsuario}`,null,null);
+
+  
+  /**
+   * @description Crea un usuario
+   * 
+   * @author Bryan Zamora
+   * @param user 
+   * @returns 
+   */
+  postCrearUsuario(user):Observable<any>{
+    return this.apiService.ApiCallSpring("POST","/usuarios/crearUsuario",user,null);
   }
-  getConsultarUsuarioDisponible(primerNombre: string, segundoNombre: string, primerApellido: string, segundoApellido: string) {
-    return this.apiService.ApiCallSpring("GET",`/usuarios/usuarioDisponible?primerNombre=${primerNombre}&segundoNombre=${segundoNombre}&primerApellido=${primerApellido}&segundoApellido=${segundoApellido}`,null,null);
+  
+  /**
+   * @description Actualiza un usuario
+   * 
+   * @author Bryan Zamora
+   * @param user 
+   * @returns 
+   */
+  putActualizarUsuario(user):Observable<any>{
+    return this.apiService.ApiCallSpring("PUT",`/usuarios/actualizarUsuario`,user,null);
   }
 
-    constructor(private apiService: ApiService,private http: HttpClient){}
+  /**
+   * @description Cambia la contrasena
+   * 
+   * @author Bryan Zamora
+   * @param contrasenia 
+   * @returns 
+   */
+  postCambiarContrasena(contrasenia):Observable<any>{
 
-    postCrearUsuario(user):Observable<any>{
-      user.password=btoa(user.password1);
-      user.photo=null;
-      return this.apiService.ApiCallSpring("POST","/usuarios/crearUsuario",user,null);
+    const objcontrasenia={
+      "contrasenia":btoa(contrasenia)
     }
 
-    postCambiarContrasena(contrasenia):Observable<any>{
+    return this.apiService.ApiCallSpring("POST","/usuarios/cambioContrasena",objcontrasenia,null);
+  }
 
-      const objcontrasenia={
-        "contrasenia":btoa(contrasenia)
-      }
-
-      return this.apiService.ApiCallSpring("POST","/usuarios/cambioContrasena",objcontrasenia,null);
-    }
-
-    putActualizarUsuario(user):Observable<any>{
-      return this.apiService.ApiCallSpring("PUT",`/usuarios/actualizarUsuario`,user,null);
-    }
-
-    getConsultaUsuario(page,perPage,valor,estado):Observable<any>{
-      return this.apiService.ApiCallSpring("GET",`/usuarios?page=${page}&perPage=${perPage}&cedulaCodigoUsuario=${valor}&estado=${estado}`,null,null);
-    }
-
-    getUsuarioXId(idUsuario):Observable<any>{
-      return this.apiService.ApiCallSpring("GET",`/usuarios/${idUsuario}/basica`,null,null);
-    }
+  /**
+   * @description Consulta el usuario por id
+   * 
+   * @author Bryan Zamora
+   * @param idUsuario 
+   * @returns 
+   */
+  getUsuarioXId(idUsuario):Observable<any>{
+    return this.apiService.ApiCallSpring("GET",`/usuarios/${idUsuario}/basica`,null,null);
+  }
 }
