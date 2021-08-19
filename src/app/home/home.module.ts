d602cee939f982d297da3d7efddd0b1000fe2728
Bryan <bryan.zamora@goitsa.me>
2021-08-19 17:26:22 -0500

    Se agrega palablas al multilenguaje

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { SeguridadComponent } from './seguridad/seguridad.component';
 import { ModulosComponent } from './modulos/modulos.component';
